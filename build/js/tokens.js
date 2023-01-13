dff132ee6ffd598daacf675bd4439bc7972c0e36
Guilherme Dias <guilhermehenriquedias@hotmail.com>
2023-01-13 08:42:28 -0300

    first commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 13 Jan 2023 11:35:51 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#6b7280",
+  "ColorGray800": "#27272a",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#8b5cf6"
+};
